66da57c3aead9a090621c3b2aa5f6dcefda301b2
Louis Kraemer <louiskraemer@gmail.com>
2017-12-08 18:51:03 +0100

    Scroll plugin

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 $(document).ready(_ => {
     $(document).scroll(_ => {
-        if($(window).scrollTop() >= 1) {
+        if ($(window).scrollTop() >= 1) {
             $('.navbar-perso').addClass('coloured');
         } else {
             $('.navbar-perso').removeClass('coloured');
         }
     })
+
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 700);
+        }
+    });
 })
